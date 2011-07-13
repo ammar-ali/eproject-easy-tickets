b91b7a5d4ad11564ec3c0215a135e55cb65cd981
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-13 09:46:06 +0000

    abc

--- trunk/Dev/Source/TicketBookDAO_EJB_BMP/src/java/ticketbook/ejb/bmp/UserRemoteHome.java
+++ trunk/Dev/Source/TicketBookDAO_EJB_BMP/src/java/ticketbook/ejb/bmp/UserRemoteHome.java
@@ -6,13 +6,8 @@
 package ticketbook.ejb.bmp;
 
 import java.rmi.RemoteException;
-import java.util.Enumeration;
-
-import javax.ejb.CreateException;
-
 import javax.ejb.EJBHome;
 import javax.ejb.FinderException;
-import ticketbook.transfer.UserTransferData;
 
 /**
  *
@@ -20,7 +15,6 @@ import ticketbook.transfer.UserTransferData;
  */
 public interface UserRemoteHome extends EJBHome {
 
-    ticketbook.ejb.bmp.UserRemote findByPrimaryKey(java.lang.Integer key)  throws FinderException, RemoteException;
-    ticketbook.ejb.bmp.UserRemote create(UserTransferData user) throws CreateException,RemoteException;
-    Enumeration findAll() throws FinderException,RemoteException;
+    ticketbook.ejb.bmp.UserRemote findByPrimaryKey(java.lang.String key)  throws FinderException, RemoteException;
+    ticketbook.ejb.bmp.UserRemote findByUsernameAndPassword(String username,String password) throws FinderException,RemoteException;
 }
