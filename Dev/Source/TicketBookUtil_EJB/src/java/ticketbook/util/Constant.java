9ff33b08fa73de810b05d06276e64ef9de924d56
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-14 06:40:10 +0000

    abc

--- Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/util/Constant.java
+++ Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/util/Constant.java
@@ -18,8 +18,9 @@ public class Constant implements Serializable {
     public static final String ID_FALSE_STRING=null;
     public static final String DEFAULT_VALUE_STRING="";
     public static final Integer DEFAULT_VALUE_INTEGER=new Integer(0);
-
     public static final ResourceBundle QUERY_DAO=PropertiesBundle.getSQLQuery();
     public static final ResourceBundle SETTING_DB_DAO=PropertiesBundle.getSQLSettingDatabase();
     public static final ResourceBundle ERROR_UTIL_EXCEPTION=PropertiesBundle.getErrorUtilException();
+    public static final Integer RECORD_NUMBER_NEED_SHOW=new Integer(10);
+    public static final Integer PAGE_NUMBER_NEED_SHOW=new Integer(5);
 }
