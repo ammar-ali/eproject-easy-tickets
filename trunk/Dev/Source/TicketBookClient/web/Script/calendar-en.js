4f3b0f46b10f08721adc324fb3249eeaa979db3f
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-18 06:04:36 +0000

    source

--- trunk/Dev/Source/TicketBookClient/web/Script/calendar-en.js
+++ trunk/Dev/Source/TicketBookClient/web/Script/calendar-en.js
@@ -1,8 +1,8 @@
-// $Id: calendar-en.js 6573 2007-03-09 08:36:16Z slip $
-// ** I18N
+/*
+ * Calendar EN language
+ * Author: Mihai Bazon, <mihai_bazon@yahoo.com>
+ */
 
-// Calendar EN language
-// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
 // Encoding: any
 // Distributed under the same terms as the calendar itself.
 
@@ -11,7 +11,7 @@
 // include your contact information in the header, as can be seen above.
 
 // full day names
-Zapatec.Calendar._DN = new Array
+Calendar._DN = new Array
 ("Sunday",
  "Monday",
  "Tuesday",
@@ -26,15 +26,15 @@ Zapatec.Calendar._DN = new Array
 // for exemplification on how one can customize the short day names, but if
 // they are simply the first N letters of the full name you can simply say:
 //
-//   Zapatec.Calendar._SDN_len = N; // short day name length
-//   Zapatec.Calendar._SMN_len = N; // short month name length
+//   Calendar._SDN_len = N; // short day name length
+//   Calendar._SMN_len = N; // short month name length
 //
 // If N = 3 then this is not needed either since we assume a value of 3 if not
 // present, to be compatible with translation files that were written before
 // this feature.
 
 // short day names
-Zapatec.Calendar._SDN = new Array
+Calendar._SDN = new Array
 ("Sun",
  "Mon",
  "Tue",
@@ -46,10 +46,10 @@ Zapatec.Calendar._SDN = new Array
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
-Zapatec.Calendar._FD = 0;
+Calendar._FD = 0;
 
 // full month names
-Zapatec.Calendar._MN = new Array
+Calendar._MN = new Array
 ("January",
  "February",
  "March",
@@ -64,7 +64,7 @@ Zapatec.Calendar._MN = new Array
  "December");
 
 // short month names
-Zapatec.Calendar._SMN = new Array
+Calendar._SMN = new Array
 ("Jan",
  "Feb",
  "Mar",
@@ -79,62 +79,50 @@ Zapatec.Calendar._SMN = new Array
  "Dec");
 
 // tooltips
-Zapatec.Calendar._TT_en = Zapatec.Calendar._TT = {};
-Zapatec.Calendar._TT["INFO"] = "About the calendar";
+Calendar._TT = {};
+Calendar._TT["INFO"] = "About the calendar";
 
-Zapatec.Calendar._TT["ABOUT"] =
+Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
-"(c) zapatec.com 2010-2020\n" + // don't translate this this ;-)
-"For latest version visit: http://www.zapatec.com/\n" +
+"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
+"For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
+"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
 "Date selection:\n" +
 "- Use the \xab, \xbb buttons to select year\n" +
 "- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
 "- Hold mouse button on any of the above buttons for faster selection.";
-Zapatec.Calendar._TT["ABOUT_TIME"] = "\n\n" +
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
 "Time selection:\n" +
 "- Click on any of the time parts to increase it\n" +
 "- or Shift-click to decrease it\n" +
 "- or click and drag for faster selection.";
 
-Zapatec.Calendar._TT["PREV_YEAR"] = "Prev. year (hold for menu)";
-Zapatec.Calendar._TT["PREV_MONTH"] = "Prev. month (hold for menu)";
-Zapatec.Calendar._TT["GO_TODAY"] = "Go Today (hold for history)";
-Zapatec.Calendar._TT["NEXT_MONTH"] = "Next month (hold for menu)";
-Zapatec.Calendar._TT["NEXT_YEAR"] = "Next year (hold for menu)";
-Zapatec.Calendar._TT["SEL_DATE"] = "Select date";
-Zapatec.Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
-Zapatec.Calendar._TT["PART_TODAY"] = " (today)";
+Calendar._TT["PREV_YEAR"] = "Prev. year (hold for menu)";
+Calendar._TT["PREV_MONTH"] = "Prev. month (hold for menu)";
+Calendar._TT["GO_TODAY"] = "Go Today";
+Calendar._TT["NEXT_MONTH"] = "Next month (hold for menu)";
+Calendar._TT["NEXT_YEAR"] = "Next year (hold for menu)";
+Calendar._TT["SEL_DATE"] = "Select date";
+Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
+Calendar._TT["PART_TODAY"] = " (today)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Zapatec.Calendar._TT["DAY_FIRST"] = "Display %s first";
+Calendar._TT["DAY_FIRST"] = "Display %s first";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
-Zapatec.Calendar._TT["WEEKEND"] = "0,6";
+Calendar._TT["WEEKEND"] = "0,6";
 
-Zapatec.Calendar._TT["CLOSE"] = "Close";
-Zapatec.Calendar._TT["TODAY"] = "Today";
-Zapatec.Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
+Calendar._TT["CLOSE"] = "Close";
+Calendar._TT["TODAY"] = "Today";
+Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
 
 // date formats
-Zapatec.Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
-Zapatec.Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
+Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
+Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
-Zapatec.Calendar._TT["WK"] = "wk";
-Zapatec.Calendar._TT["TIME"] = "Time:";
-
-Zapatec.Calendar._TT["E_RANGE"] = "Outside the range";
-
-Zapatec.Calendar._TT._AMPM = {am : "am", pm : "pm"};
-
-/* Preserve data */
-	if(Zapatec.Calendar._DN) Zapatec.Calendar._TT._DN = Zapatec.Calendar._DN;
-	if(Zapatec.Calendar._SDN) Zapatec.Calendar._TT._SDN = Zapatec.Calendar._SDN;
-	if(Zapatec.Calendar._SDN_len) Zapatec.Calendar._TT._SDN_len = Zapatec.Calendar._SDN_len;
-	if(Zapatec.Calendar._MN) Zapatec.Calendar._TT._MN = Zapatec.Calendar._MN;
-	if(Zapatec.Calendar._SMN) Zapatec.Calendar._TT._SMN = Zapatec.Calendar._SMN;
-	if(Zapatec.Calendar._SMN_len) Zapatec.Calendar._TT._SMN_len = Zapatec.Calendar._SMN_len;
-	Zapatec.Calendar._DN = Zapatec.Calendar._SDN = Zapatec.Calendar._SDN_len = Zapatec.Calendar._MN = Zapatec.Calendar._SMN = Zapatec.Calendar._SMN_len = null
+Calendar._TT["WK"] = "wk";
+Calendar._TT["TIME"] = "Time:";
