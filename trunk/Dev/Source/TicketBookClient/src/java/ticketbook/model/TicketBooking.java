02365e3943ff577c027da11372733a4bc8002db0
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-23 03:12:15 +0000

    source

--- trunk/Dev/Source/TicketBookClient/src/java/ticketbook/model/TicketBooking.java
+++ trunk/Dev/Source/TicketBookClient/src/java/ticketbook/model/TicketBooking.java
@@ -11,8 +11,10 @@ import javax.ejb.CreateException;
 import javax.ejb.FinderException;
 import ticketbook.ejb.bmp.TicketBookingRemote;
 import ticketbook.ejb.bmp.TicketBookingRemoteHome;
+import ticketbook.ejb.bmp.TicketRemoteHome;
 import ticketbook.transfer.TicketBookingTransferData;
 import ticketbook.util.TicketBookLookUpJNDI;
+import ticketbook.util.TicketBookParameter;
 
 /**
  *
@@ -42,5 +44,30 @@ public class TicketBooking {
             ex.printStackTrace();
         }
         return new ArrayList();
+    }
+
+    public static TicketBookingRemote getTicketBookingByID(Integer ID){
+        try {
+            TicketBookingRemoteHome home = TicketBookLookUpJNDI.getTicketBookingRemoteHome();
+            return home.findByPrimaryKey(ID);
+        } catch (FinderException ex) {
+            ex.printStackTrace();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Integer countTicketBookingByStatus(String status){
+        try {
+            TicketBookingRemoteHome home = TicketBookLookUpJNDI.getTicketBookingRemoteHome();
+            TicketBookingRemote ticket= home.create(null);
+            if(status==null||status.equals(""))
+                status=TicketBookParameter.getInstance().getNewStatusTicketBooking();
+            return ticket.countTicketBookingByStatus(status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } 
+        return new Integer(0);
     }
 }
