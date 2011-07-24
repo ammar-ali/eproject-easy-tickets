1455f246226a527763b43a221d9f6e837e6dacd5
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-24 10:23:52 +0000

    source

---
+++ Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/transfer/EventTransferData.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ticketbook.transfer;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+public class EventTransferData implements Serializable {
+    private String title;
+    private String content;
+    private String artist;
+}
