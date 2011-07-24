9bf5112d44bd1455a9cf0f8a0778e9b663abcc54
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-24 10:23:52 +0000

    source

---
+++ trunk/Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/transfer/VenueTransferData.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ticketbook.transfer;
+
+import java.io.Serializable;
+import ticketbook.util.Constant;
+
+/**
+ *
+ * @author Admin
+ */
+public class VenueTransferData implements Serializable {
+    private Integer ID;
+    private String name;
+    private String address;
+
+    public VenueTransferData(){
+        this.ID=Constant.DEFAULT_VALUE_INTEGER;
+        this.name=Constant.DEFAULT_VALUE_STRING;
+        this.address=Constant.DEFAULT_VALUE_STRING;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+}
