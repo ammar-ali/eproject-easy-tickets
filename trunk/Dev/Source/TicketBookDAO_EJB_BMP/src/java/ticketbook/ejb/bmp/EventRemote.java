2100128bb31b698fc3ed77d51630cdd90ca24947
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-26 03:21:33 +0000

    source

---
+++ trunk/Dev/Source/TicketBookDAO_EJB_BMP/src/java/ticketbook/ejb/bmp/EventRemote.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ticketbook.ejb.bmp;
+
+import java.rmi.RemoteException;
+import javax.ejb.EJBObject;
+
+/**
+ *
+ * @author Admin
+ */
+public interface EventRemote extends EJBObject {
+    public Integer getID() throws RemoteException;
+    public String getArtist() throws RemoteException;
+    public Integer getCityID() throws RemoteException  ;
+    public String getContent()  throws RemoteException;
+    public Integer getEventTypeID()  throws RemoteException;
+    public String getImage()  throws RemoteException;
+    public String getTitle()  throws RemoteException;
+    public Integer getVenueID()  throws RemoteException;
+    public void setID(Integer ID)  throws RemoteException;
+    public void setArtist(String artist)  throws RemoteException;
+    public void setCityID(Integer cityID)  throws RemoteException;
+    public void setContent(String content)  throws RemoteException;
+    public void setEventTypeID(Integer eventTypeID)  throws RemoteException;
+    public void setImage(String image)  throws RemoteException;
+    public void setTitle(String title)   throws RemoteException;
+    public void setVenueID(Integer venueID)  throws RemoteException;
+}
