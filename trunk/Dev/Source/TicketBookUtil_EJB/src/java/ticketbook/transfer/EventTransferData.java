3525c5b77e39e6d7a83e0118922b9853d568af65
phamhongtam27989 <phamhongtam27989@afa057c3-1545-2698-daf7-cf91c6c8bc53>
2011-07-29 16:40:03 +0000

    source

--- trunk/Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/transfer/EventTransferData.java
+++ trunk/Dev/Source/TicketBookUtil_EJB/src/java/ticketbook/transfer/EventTransferData.java
@@ -21,6 +21,10 @@ public class EventTransferData implements Serializable {
     private Integer eventTypeID;
     private Integer venueID;
     private Integer cityID;
+    private String eventTypeName;
+    private String venueName;
+    private String cityName;
+    private String venueAddress;
     public EventTransferData(){
         this.ID=Constant.ID_FALSE_INTETER;
         this.title=Constant.DEFAULT_VALUE_STRING;
@@ -30,7 +34,45 @@ public class EventTransferData implements Serializable {
         this.eventTypeID=Constant.ID_FALSE_INTETER;
         this.image=Constant.DEFAULT_VALUE_STRING;
         this.venueID=Constant.ID_FALSE_INTETER;
+        this.eventTypeName=Constant.DEFAULT_VALUE_STRING;
+        this.venueName=Constant.DEFAULT_VALUE_STRING;
+        this.cityName=Constant.DEFAULT_VALUE_STRING;
+        this.venueAddress=Constant.DEFAULT_VALUE_STRING;
     }
+
+    public String getVenueAddress() {
+        return venueAddress;
+    }
+
+    public void setVenueAddress(String venueAddress) {
+        this.venueAddress = venueAddress;
+    }
+
+    
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getEventTypeName() {
+        return eventTypeName;
+    }
+
+    public void setEventTypeName(String eventTypeName) {
+        this.eventTypeName = eventTypeName;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
     public Integer getID() {
         return ID;
     }
